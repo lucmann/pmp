e3bba625a177f62251dcbcf6bb861fc4e366de32
Luc Ma <luc@sietium.com>
2025-01-02 10:18:28 +0800

    script: [stress-test.sh] add signal trap
    Signed-off-by: Luc Ma <luc@sietium.com>

--- scripts/shell/stress-test.sh
+++ scripts/shell/stress-test.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 # Start 4 instances of glmark2 in the background
+
+signal_handler()
+{
+    echo "Caught signal, killing all glmark2 instances"
+    kill $PID_1 $PID_2 $PID_3 $PID_4
+    exit 1
+}
+
+trap signal_handler SIGINT SIGTSTP
+
 for i in {1..4}; do
-    eval "glmark2 -b build >$i.log 2>&1 & PID_$i=\$!"
+    eval "glmark2 -b build --run-forever >$i.log 2>&1 & PID_$i=\$!"
     echo "Started glmark2 with PID $! and logging to $i.log"
 done
 
