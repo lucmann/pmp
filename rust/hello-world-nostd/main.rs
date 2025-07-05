624305619823f4e5e3c9a7fb035246a19088bfca
Luc Ma <luc@sietium.com>
2025-07-05 12:20:10 +0800

    rust: hello-world-nostd
    It ends up that syscalls on their own will clobber these registers (rcx,
    r11) on x86_64
    
    Signed-off-by: Luc Ma <luc@sietium.com>

--- rust/hello-world-nostd/main.rs
+++ rust/hello-world-nostd/main.rs
@@ -14,7 +14,6 @@ pub unsafe extern "C" fn _start() {
     let message = b"Hello, world!\n";
     let fd: usize = 1; // File descriptor for stdout
     let syscall_no: usize = 1; // Syscall number for write in Linux x86_64
-
     // Use inline asm macro to perform the syscall
     core::arch::asm!(
         "syscall",
@@ -22,6 +21,8 @@ pub unsafe extern "C" fn _start() {
         in("rdi") fd,         // file descriptor
         in("rsi") message.as_ptr(), // pointer to the message
         in("rdx") message.len(), // length of the message
+        out("rcx") _, out("r11") _, // telling compiler these registers will
+                                    // be clobbered by syscalls
     );
 
     // Exit the program with status code 0
@@ -29,5 +30,7 @@ pub unsafe extern "C" fn _start() {
         "syscall",
         in("rax") 60, // syscall number for exit
         in("rdi") 0,  // exit status code
+        out("rcx") _, out("r11") _, // telling compiler these registers will
+                                    // be clobbered by syscalls
     );
 }
